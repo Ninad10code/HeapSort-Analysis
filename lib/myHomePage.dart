768a522cebe9fee001ba36857a24f8683cbe59cf
unknown <ninadmsn@outlook.com>
2020-06-24 18:20:23 +0530

    Home page template created

--- lib/myHomePage.dart
+++ lib/myHomePage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -6,14 +7,71 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
+  String input;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Heap Sort Analysis'),
-        backgroundColor: Colors.black54,
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        margin: EdgeInsets.all(10.0),
+        child: Column(
+//          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(5.0),
+              padding: EdgeInsets.all(10.0),
+
+//              color: Colors.purple,
+              child: Text(
+                  'Enter the input',
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    letterSpacing: 2.0,
+
+                  ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(5.0),
+              padding: EdgeInsets.all(10.0),
+//              color: Colors.purple,
+
+              child: TextFormField(
+                decoration: InputDecoration(
+//                    labelText: 'Enter your username',
+                hintText: 'Enter here',
+                  focusColor: Colors.amber,
+
+
+                ),
+                onChanged: (String value){
+                  input=value;
+                },
+              ),
+
+
+            ),
+            Container(
+              child: FlatButton(
+                onPressed: (){
+                  print('button is clicked');
+                },
+                child: Text(
+                    'Submit',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                color: Color(0XFF00cd00),
+              )
+            ),
+          ],
+        ),
       ),
     );
   }
