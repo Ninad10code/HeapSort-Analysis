c12a9639401092d9d174e00080c2e199f1d7845b
unknown <ninadmsn@outlook.com>
2020-06-26 03:39:44 +0530

    layout and display of fetched ascii values done

--- lib/buildHeap.dart
+++ lib/buildHeap.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,24 +13,23 @@ import 'package:flutter/material.dart';
 //  }
 //}
 class BuildHeap extends StatefulWidget {
-  BuildHeap({this.input,this.length,this.array});
-   final String input;
-   final int length;
-   final List array;
-
-  @override
-  _BuildHeapState createState() => _BuildHeapState(input: input,length: length);
-}
-
-class _BuildHeapState extends State<BuildHeap> {
-
-  _BuildHeapState({this.input,this.length,this.array});
+  BuildHeap({this.input, this.length, this.array});
   final String input;
   final int length;
   final List array;
 
+  @override
+  _BuildHeapState createState() =>
+      _BuildHeapState(input: input, length: length, array: array);
+}
 
-  int index=0;
+class _BuildHeapState extends State<BuildHeap> {
+  _BuildHeapState({this.input, this.length, this.array});
+  final String input;
+  final int length;
+  final List array;
+
+  int index = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,25 +57,81 @@ class _BuildHeapState extends State<BuildHeap> {
             Container(
               margin: EdgeInsets.all(5.0),
               padding: EdgeInsets.all(10.0),
-              child: Text(
-                'Step$index',
-                style: TextStyle(
-                  letterSpacing: 1.0,
-                  fontSize: 15.0,
-                  color: Colors.white,
+              decoration: BoxDecoration(
+                  color: Colors.purple,
+                  borderRadius: BorderRadiusDirectional.circular(4.0)),
+              child: Center(
+                child: Text(
+                  'Step$index',
+                  style: TextStyle(
+                    letterSpacing: 1.0,
+                    fontSize: 15.0,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-              color: Colors.purple,
+//              color: Colors.purple,
             ),
             Container(
-              child: Row(
-                children: <Widget>[
-                  Expanded(
+              margin: EdgeInsets.all(5.0),
+              padding: EdgeInsets.all(10.0),
+              child: Text(array[index].toString()),
+            ),
+            Container(
+                height: 80.0,
 
-                  )
-                ],
-              ),
-            )
+                child: ListView(
+
+                  scrollDirection: Axis.horizontal,
+                  children: List.generate(length, (pin) {
+                    return Container(
+                      width: 80.0,
+                      height: 80.0,
+                      padding: EdgeInsets.all(20.0),
+                      margin: EdgeInsets.all(2.0),
+                      color: Colors.blue,
+                      child: Text(
+                        array[pin].toString(),
+                        textAlign: TextAlign.center,
+
+                        style: TextStyle(
+                          color: Colors.white,
+                          letterSpacing: 1.0,
+                          fontSize: 20.0,
+
+                        ),
+                      ),
+                    );
+                  }),
+                )),
+            Container(
+                margin: EdgeInsets.all(5.0),
+                padding: EdgeInsets.all(10.0),
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      index++;
+                    });
+                    print('objectnext button is pressed');
+                  },
+                  child: Container(
+                    margin: EdgeInsets.all(5.0),
+                    padding: EdgeInsets.all(10.0),
+//
+                    decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadiusDirectional.circular(4.0)),
+                    child: Text(
+                      'next',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                        letterSpacing: 1.0,
+                      ),
+                    ),
+                  ),
+                ))
           ],
         ),
       ),
