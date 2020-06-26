d8c97194adfde2e641ac0cd658c618caa369608b
unknown <ninadmsn@outlook.com>
2020-06-26 17:05:45 +0530

    Visibility status added to check whether build is complete

--- lib/buildHeap.dart
+++ lib/buildHeap.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'buildSort.dart';
 
 //class BuilHeapPage extends StatelessWidget {
 //  BuildHeapPage({@required this.input});
@@ -27,9 +28,18 @@ class _BuildHeapState extends State<BuildHeap> {
   _BuildHeapState({this.input, this.length, this.array});
   final String input;
   final int length;
-  final List array;
+  List array;
+  BuildSort heapify = BuildSort();
 
   int index = 0;
+
+  bool validate = true;
+  void nextStep() {
+    if (index == length - 3) {
+      validate = false;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -73,15 +83,27 @@ class _BuildHeapState extends State<BuildHeap> {
 //              color: Colors.purple,
             ),
             Container(
-              margin: EdgeInsets.all(5.0),
-              padding: EdgeInsets.all(10.0),
-              child: Text(array[index].toString()),
-            ),
+                margin: EdgeInsets.all(5.0),
+                padding: EdgeInsets.all(10.0),
+                child: Visibility(
+                  visible: heapify.isBuildComplete(array,index, length),
+                  child: Text(
+                      'Build is complete',
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontWeight: FontWeight.w600,
+                      letterSpacing: 1.0,
+
+//                      backgroundColor: Colors.green,
+
+
+                    ),
+
+                  ),
+                )),
             Container(
                 height: 80.0,
-
                 child: ListView(
-
                   scrollDirection: Axis.horizontal,
                   children: List.generate(length, (pin) {
                     return Container(
@@ -93,12 +115,10 @@ class _BuildHeapState extends State<BuildHeap> {
                       child: Text(
                         array[pin].toString(),
                         textAlign: TextAlign.center,
-
                         style: TextStyle(
                           color: Colors.white,
                           letterSpacing: 1.0,
                           fontSize: 20.0,
-
                         ),
                       ),
                     );
@@ -107,31 +127,63 @@ class _BuildHeapState extends State<BuildHeap> {
             Container(
                 margin: EdgeInsets.all(5.0),
                 padding: EdgeInsets.all(10.0),
-                child: GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      index++;
-                    });
-                    print('objectnext button is pressed');
-                  },
-                  child: Container(
-                    margin: EdgeInsets.all(5.0),
-                    padding: EdgeInsets.all(10.0),
+                child: Visibility(
+                  visible: !heapify.isBuildComplete(array, index, length),
+                  child: GestureDetector(
+                    onTap: () {
+                      nextStep();
+                      setState(() {
+                        if (validate) {
+                          array = heapify.iterateFunction(array, length, index);
+                          index++;
+                        }
+                      });
+                      print('objectnext button is pressed');
+                    },
+                    child: Container(
+                      margin: EdgeInsets.all(5.0),
+                      padding: EdgeInsets.all(10.0),
 //
-                    decoration: BoxDecoration(
-                        color: Colors.green,
-                        borderRadius: BorderRadiusDirectional.circular(4.0)),
-                    child: Text(
-                      'next',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        color: Colors.white,
-                        letterSpacing: 1.0,
+                      decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadiusDirectional.circular(4.0)),
+                      child: Text(
+                        'next',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                          letterSpacing: 1.0,
+                        ),
                       ),
                     ),
                   ),
-                ))
+                ),),
+            Container(
+              child: Visibility(
+                visible: heapify.isBuildComplete(array, index, length),
+                child: FlatButton(
+                  onPressed: () {
+
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+
+                          return ;
+                        },
+                      ),
+                    );
+                  },
+                  child: Text(
+                    'Proceed to sort',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  color: Color(0XFF00cd00),
+                ),),
+            )
           ],
         ),
       ),
